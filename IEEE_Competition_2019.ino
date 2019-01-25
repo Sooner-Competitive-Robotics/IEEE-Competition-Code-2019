855e7c8beade55b2aeee0380252407b47f8832db
Kichlids <32001401+Kichlids@users.noreply.github.com>
2019-01-24 18:25:11 -0600

    Pseudo code

---
+++ IEEE_Competition_2019.ino
@@ -0,0 +1,40 @@
+#include "IEEErobot2019.h"
+#include "StepperMotorDrivetrain.h"
+
+void setup()
+{
+	Serial.begin(9600);
+	
+	// Initialize motors and basic drivetrain functions
+	robotSetup();
+}
+
+void loop()
+{
+	// Read JSON file and locate cubes
+	
+	
+	// Drive around the field until mothership is found
+	
+	
+	// Identify one of the letters on the mothership, which identifies the rest of positions of the letters
+	
+	
+	// Go to a cube location
+	
+	
+	// Pick up cube
+	
+	
+	// Determine letter on cube
+	
+	
+	// Drop off cube
+	
+	
+	// Drive back to the starting point and turn the light on
+	
+	
+	// End of program
+	while(true){};
+}
